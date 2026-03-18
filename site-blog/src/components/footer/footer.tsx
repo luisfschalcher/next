b78f6c650812f9580d248a0e22c782c2652a8640
Luis Felipe Schalcher de Oliveira <lfsoliveira2005@gmail.com>
2026-03-18 09:52:51 -0300

    adição do footer

--- site-blog/src/components/footer/footer.tsx
+++ site-blog/src/components/footer/footer.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
+
 export const Footer = () => {
     return (
-        <footer></footer>
+        <footer className="border-t border-white/10">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between md:flex-row gap-8 py-8">
+                    <Link href="/">
+                        <Image src='/vercel.svg' alt='logo' width={32} height={32}></Image>
+                    </Link>
+                    <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
+                        <Link href="/termos-de-uso" className="hover:text-primary">Termos de uso</Link>
+                        <Link href="/politica-de-privacidade" className="hover:text-primary">Política de privacidade</Link>
+                        <Link href="/feedback" className="hover:text-primary">Feedback</Link>
+                    </nav>
+                </div>
+            </div>
+        </footer>
     )
 }
