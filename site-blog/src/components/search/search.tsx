2cc02aed8f054c1517a34e5ebe9afeb8592229c9
dcrkp2181@gmail.com <129186009+luisfschalcher@users.noreply.github.com>
2026-04-09 11:51:22 -0300

    componente search (parte 2)

--- site-blog/src/components/search/search.tsx
+++ site-blog/src/components/search/search.tsx
@@ -5,18 +5,24 @@ import { cn } from "@/lib/utils"
 
 export const Search = () => {
     const router = useRouter()
-    const query = router.query.q as string
+    const query = (router.query.q as string) ?? ''
 
-    const handleSearch = useCallback((event: React.FormEvent)=>{
+    const handleSearch = useCallback((event: React.FormEvent) => {
         event.preventDefault()
         if (query.trim()){
             router.push(`/blog?q=${encodeURIComponent(query)}`)
         }
     }, [query, router])
 
-    const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newQuery = event.target.value
         router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
+            shallow: true, scroll: false
+        })
+    }
+
+    const resetSearch = () => {
+        router.push('/blog', undefined, {
             shallow: true, scroll: false
         })
     }
@@ -24,8 +30,8 @@ export const Search = () => {
     return (
         <form onSubmit={handleSearch} className="relative group w-full md:w-60">
             <SearchIcon className={cn('text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300', query?'text-blue-300':'')}></SearchIcon>
-            <input type="text" placeholder="Buscar" onChange={handleQueryChange} className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"/>
-            <CircleX className="absolute w-4 h-4 top-1/2 -translate-y-1/2 right-3 text-gray-300"></CircleX>
+            <input value={query} type="text" placeholder="Buscar" onChange={handleQueryChange} className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"/>
+            {query && (<CircleX onClick={resetSearch} className="absolute w-4 h-4 top-1/2 -translate-y-1/2 right-3 text-gray-300"></CircleX>)}
         </form>
     )
 }
