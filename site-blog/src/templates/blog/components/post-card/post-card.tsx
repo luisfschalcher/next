9966e05ba964993aed4157cdd8fe76afd46e995d
luisfschalcher <129186009+luisfschalcher@users.noreply.github.com>
2026-04-14 17:15:03 -0300

    postCard (parte 2)

--- site-blog/src/templates/blog/components/post-card/post-card.tsx
+++ site-blog/src/templates/blog/components/post-card/post-card.tsx
@@ -2,36 +2,51 @@ import Link from "next/link"
 import Image from "next/image"
 import elon from "@/assets/elon.webp"
 
-export const PostCard = () => {
+type Author = {
+    name: string
+    avatar: string
+}
+
+type  PostCardProps = {
+    slug: string
+    title: string
+    description: string
+    image: string
+    date: string
+    author: Author
+}
+
+export const PostCard = ({slug, title, description, image, date, author}: PostCardProps) => {
     return (
-        <Link href={`/blog/`} className="w-full max-w-2xl rounded-3xl border-[1px] border-gray-400 bg-gray-600 overflow-hidden transition-all duration-300 hover:border-blue-300">
+        <Link href={`/blog/${slug}`} className="w-full max-w-2xl rounded-3xl border-[1px] border-gray-400 bg-gray-600 overflow-hidden transition-all duration-300 hover:border-blue-300">
             <div className="p-2 rounded-md overflow-hidden">
                 {/*Imagem*/}
                 <div className="relative">
-                    <div className="absolute top-0 right-0 px-3 py-1">
-                        <span className="text-gray-300 text-body-xs">20/12/2024</span>
+                    {/*Data*/}
+                    <div className="absolute top-0 right-0 px-3 py-1 bg-gray-600 backdrop-blur-sm rounded-bl-[10px]">
+                        <span className="text-gray-300 text-body-xs">{date}</span>
                     </div>
                     <Image
-                        src={elon}
-                        alt="elon"
+                        src={image}
+                        alt=""
                         width={288}
                         height={144}
-                        className="w-full h-40 object-cover object-center"></Image>
+                        className="w-full h-40 object-cover object-center rounded-t-2xl"></Image>
                 </div>
                 {/*Info*/}
                 <div className="px-2 mt-4 space-y-4">
-                    <h2 className="text-heading-sm text-gray-100 line-clamp-3">elon</h2>
-                    <p className="text-gray-300 text-body-sm line-clamp-3">elon</p>
+                    <h2 className="text-heading-sm text-gray-100 line-clamp-3">{title}</h2>
+                    <p className="text-gray-300 text-body-sm line-clamp-3">{description}</p>
                     {/*Footer*/}
                     <div className="flex items-center gap-3 border-t border-gray-400 py-4">
                         <div className="relative h-5 w-5 md:h-6 md:w-6 overflow-hidden rounded-full border-blue-200 border-[1px]">
                             <Image
-                                src={elon}
-                                alt="elon"
+                                src={author.avatar}
+                                alt=""
                                 fill
                                 className="object-cover rounded-md"></Image>
                         </div>
-                        <span className="text-body-sm text-gray-300">elon</span>
+                        <span className="text-body-sm text-gray-300">{author.name}</span>
                     </div>
                 </div>
             </div>
