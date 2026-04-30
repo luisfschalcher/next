8bfc5d0d53b9540a67cad838f9cc8456c6fc8f8c
dcrkp2181@gmail.com <129186009+luisfschalcher@users.noreply.github.com>
2026-04-30 11:22:48 -0300

    finalização da página post e componente avatar

--- site-blog/src/pages/blog/[slug].tsx
+++ site-blog/src/pages/blog/[slug].tsx
@@ -1,11 +1,56 @@
 import { useRouter } from "next/router"
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import Link from "next/link"
+import { allPosts } from "contentlayer/generated"
+import Image from "next/image"
+import { Avatar } from "@/components/avatar"
 
 export default function PostPage() {
     const router = useRouter()
-    const slug = router.query.slug as string
+    const slug = typeof router.query.slug === "string" ? router.query.slug : ""
+    const post = allPosts.find(
+        (post) => post.slug?.toLowerCase() === slug.toLowerCase()
+    )!
+    const publishedDate = new Date(post?.date).toLocaleDateString('pt-Br')
     return (
-        <div className="mt-32">
-            <h2 className="text-gray-100">Post Page</h2>
-        </div>
+        <main className="mt-32 text-gray-100">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild className="text-action-sm">
+                            <Link href="/blog">Blog</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator></BreadcrumbSeparator>
+                    <BreadcrumbItem>
+                        <span className="text-blue-200 text-action-sm">{post?.title}</span>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
+                <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
+                    <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+                        <Image src={post?.image ?? ''} alt={post?.title ?? ''} fill className="object-cover"></Image>
+                    </figure>
+                    <header className="p-4 md:p-6 lg:p-12 pb-0">
+                        <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+                            {post?.title}
+                        </h1>
+                        <Avatar.Container>
+                            <Avatar.Image src={post?.author.avatar.trim()} alt={post?.title} />
+                            <Avatar.Content>
+                                <Avatar.Title>
+                                    {post?.author.name}
+                                </Avatar.Title>
+                                <Avatar.Description>
+                                    Publicado em {" "}
+                                    <time dateTime={post?.date}>{publishedDate}</time>
+                                </Avatar.Description>
+                            </Avatar.Content>
+                        </Avatar.Container>
+                    </header>
+                </article>
+            </div>
+        </main>
     )
 }
