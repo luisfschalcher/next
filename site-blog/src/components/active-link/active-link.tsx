b553e55b88cfc647cb357c6579e634a2e80982dc
luisfschalcher <129186009+luisfschalcher@users.noreply.github.com>
2026-05-11 20:59:31 -0300

    migração para app router

--- site-blog/src/components/active-link/active-link.tsx
+++ site-blog/src/components/active-link/active-link.tsx
@@ -1,16 +1,19 @@
+'use client'
 import Link, { LinkProps } from "next/link"
 import { cn } from "@/lib/utils"
-import { useRouter } from "next/router"
+import { usePathname } from "next/navigation"
 
 type ActiveLinkProps = {
     children: React.ReactNode
 } & LinkProps
 
-export const ActiveLink = ({children, href, ...rest}: ActiveLinkProps) => {
-    const router = useRouter()
-    const isCurrentPath = router.asPath === href || router.asPath === rest.as
+export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
+    const linkPath = (typeof href === 'string' ? href : href.pathname) ?? ''
+    const pathname = usePathname()
+    const isActive = linkPath === pathname || pathname?.startsWith(`${linkPath}/`)
+
     return (
-        <Link href={href} className={cn('text-action-sm transition-colors hover:text-gray-200',
-            isCurrentPath ? 'text-blue-200 hover:text-blue-300' : 'text-gray-300')}>{children}</Link>
+        <Link {...rest} href={href} className={cn('text-action-sm transition-colors hover:text-gray-200',
+            isActive ? 'text-blue-200 hover:text-blue-300' : 'text-gray-300')}>{children}</Link>
     )
 }
