4227a3f0ddeae4403e7e949087ce34243e982e65
Luis Felipe Schalcher de Oliveira <129186009+luisfschalcher@users.noreply.github.com>
2026-08-03 10:24:46 -0300

    melhorando o SEO da laning page

--- site-blog/src/app/page.tsx
+++ site-blog/src/app/page.tsx
@@ -1,4 +1,27 @@
 import { LandingPage } from "@/templates/landing-page";
+import type {Metadata} from "next"
+
+export const metadata: Metadata = {
+    title: 'Site.Set',
+    description: 'Venda seus produtos...',
+    robots: 'index, follow',
+    openGraph: {
+        title: 'Site.Set',
+        description: 'venda seus produtos...',
+        url: 'https://site-blog-vert.vercel.app/',
+        siteName: 'Site.Set',
+        locale: 'pt_BR',
+        type: 'website',
+        images: [
+            {
+                url: 'https://site-blog-vert.vercel.app/og-image.webp',
+                width: 800,
+                height: 600,
+                alt: 'Site.Set'
+            }
+        ]
+    }
+}
 
 export default function HomePage() {
     return (
